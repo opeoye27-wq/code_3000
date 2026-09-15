878cbac98e5cc2f3feb59ee5904987a47fd94a91
Opeyemi <opeoye27@gmail.com>
2026-09-14 22:27:06 -0400

    HW 2

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,11 +10,11 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=10000, #0.1
+        learning_rate=0.1, #0.1
         n_estimators=100, #100
-        max_depth=1, #8
+        max_depth=2, #8
         subsample=1, #1
-        min_samples_leaf=100, #1
+        min_samples_leaf=1, #1
         random_state=seed
     )
     model.fit(X, y)
